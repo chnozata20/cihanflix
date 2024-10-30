9637f142e704512906c17a24bfbb4f686ea6c76a
Cihan Ozata <cihan.ozata@INSTRPSD1099.local>
2024-10-30 18:59:28 +0300

    grid-col-1-mobil

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -83,7 +83,7 @@ export default function Category() {
                     </button>
                 </div>
             </div>
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center items-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center items-center">
                 {(isMovie ? currentDisplayedMovies : currentDisplayedTVShows).map((item) => (
                     <MediaPoster key={item.id} item={item} />
                 ))}
